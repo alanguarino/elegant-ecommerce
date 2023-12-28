313dfda2b137f86f180aea75b07d7225c78194af
alanguarino <alaan.guarino@gmail.com>
2023-12-28 20:35:32 -0300

    navbar styling finished and resposive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { useRef, useContext } from "react";
+import { useRef, useContext, useState } from "react";
 import CartModal from "./CartModal.jsx";
 import { CartContext } from "../store/shopping-cart-context.jsx";
 
@@ -23,49 +23,67 @@ export default function Header() {
     );
   }
 
+  let [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  let Links = [
+    { name: "Home", link: "/" },
+    { name: "Products", link: "/" },
+    { name: "Contact", link: "/" },
+  ];
+
   return (
     <>
       <CartModal ref={modal} title="Your Cart" actions={modalActions} />
-      <header className="w-full fixed bg-gradient-to-r from-gray-800 via-gray-700 to-gray-800 text-gray-100 min-w-screen shadow-md">
-        <div className="container mx-auto p-4">
-          <div className="flex items-center justify-between">
-            <div className="flex items-center space-x-2">
-              <img src="logo.png" alt="Elegant model" className="w-8 h-auto" />
-              <div>
-                <h1 className="text-xl font-bold">
-                  Elegant <Shop></Shop>
-                </h1>
-                <p className="text-sm text-gray-300">Your first choice</p>
-              </div>
-            </div>
-            <nav>
-              <ul className="flex space-x-4">
-                <li>
-                  <a href="#" className="hover:text-yellow-400">
-                    Home
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="hover:text-yellow-400">
-                    Products
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="hover:text-yellow-400">
-                    Contact
-                  </a>
-                </li>
-              </ul>
-            </nav>
+
+      {/* className="fixed left-0 right-0 bg-gradient-to-r from-gray-800 via-gray-700 to-gray-800 text-gray-100" */}
+      <div className="shadow-md w-full fixed top-0 left-0">
+        <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
+          <div
+            className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
+      text-gray-800"
+          >
+            <span className="w-8 h-8 md:items-center mt-2 mr-4">
+              <img
+                src="logo.png"
+                alt="Elegant model"
+                // className="w-8 h-8 md:items-center mt-2 mr-4"
+              />
+            </span>
+            Elegant Shop
+          </div>
+
+          <div
+            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+            className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
+          >
+            <ion-icon name={isMobileMenuOpen ? "close" : "menu"}></ion-icon>
+          </div>
+
+          <ul
+            className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+              isMobileMenuOpen ? "top-20 " : "top-[-490px]"
+            }`}
+          >
+            {Links.map((link) => (
+              <li
+                key={link.name}
+                className="md:mr-8 md:hover:border-b-2 text-xl md:my-0 my-7"
+              >
+                <a href={link.link} className="text-gray-800 duration-500">
+                  {link.name}
+                </a>
+              </li>
+            ))}
+
             <button
               onClick={handleOpenCartClick}
-              className="bg-yellow-400 rounded px-4 py-2 text-gray-800 hover:bg-yellow-500"
+              className="flex md:justify-between bg-yellow-400 rounded px-4 py-2 text-gray-800 hover:bg-yellow-500"
             >
               Cart ({cartQuantity})
             </button>
-          </div>
+          </ul>
         </div>
-      </header>
+      </div>
     </>
   );
 }
