6de21e14ba880f5552e8ac6d621e561226808582
alanguarino <alaan.guarino@gmail.com>
2023-12-28 01:15:23 -0300

    header to Tailwind styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,14 +26,45 @@ export default function Header() {
   return (
     <>
       <CartModal ref={modal} title="Your Cart" actions={modalActions} />
-      <header id="main-header">
-        <div id="main-title">
-          <img src="logo.png" alt="Elegant model" />
-          <h1>Elegant Context</h1>
+      <header className="w-full fixed bg-gradient-to-r from-gray-800 via-gray-700 to-gray-800 text-gray-100 min-w-screen shadow-md">
+        <div className="container mx-auto p-4">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center space-x-2">
+              <img src="logo.png" alt="Elegant model" className="w-8 h-auto" />
+              <div>
+                <h1 className="text-xl font-bold">
+                  Elegant <Shop></Shop>
+                </h1>
+                <p className="text-sm text-gray-300">Your first choice</p>
+              </div>
+            </div>
+            <nav>
+              <ul className="flex space-x-4">
+                <li>
+                  <a href="#" className="hover:text-yellow-400">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-yellow-400">
+                    Products
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-yellow-400">
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </nav>
+            <button
+              onClick={handleOpenCartClick}
+              className="bg-yellow-400 rounded px-4 py-2 text-gray-800 hover:bg-yellow-500"
+            >
+              Cart ({cartQuantity})
+            </button>
+          </div>
         </div>
-        <p>
-          <button onClick={handleOpenCartClick}>Cart ({cartQuantity})</button>
-        </p>
       </header>
     </>
   );
